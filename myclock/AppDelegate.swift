7b63a4a8c80497f938d8e8155ab149f829389157
administrator <administrator@ringo-001.local>
2018-05-16 13:55:46 +0900

    Initial Commit

---
+++ myclock/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  myclock
+//
+//  Created by administrator on 2018/05/16.
+//  Copyright © 2018年 administrator. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ myclock/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  myclock
+//
+//  Created by administrator on 2018/05/16.
+//  Copyright © 2018年 administrator. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
